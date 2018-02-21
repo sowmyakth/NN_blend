881ef68e6dbc067f120e61a68eba98f9d08caa0b
sowmyakth <sowmya.spacebug@gmail.com>
2018-02-21 14:31:41 -0800

    added some parts of multi layer net

--- scripts/basic_net_utils.py
+++ scripts/basic_net_utils.py
@@ -5,26 +5,25 @@ import numpy as np
 import math
 import tensorflow as tf
 
+def get_deconv_layer():
 
 class CNN_deblender(object):
     """Class to initialize and run CNN"""
     def __init__(self, ):
         self.optimizer = tf.train.AdamOptimizer(5e-4)
         self.sess = tf.Session()
-        self.build_simple_model()
+        self.build_net()
         self.sess.run(tf.global_variables_initializer())
 
     def get_mean_loss(self, y, y_out):
         total_loss = tf.nn.l2_loss(self.y - self.y_out)
         self.mean_loss = tf.reduce_mean(total_loss)
 
-    def build_simple_model(self):
+    def simple_model(self):
         """makes a simple 2 layer CNN
         layer 1 Conv 5*5*2s2, 256/ReLU
         layer 2 FC 32*32,1, 49
          """
-        self.X = tf.placeholder(tf.float32, [None, 32, 32, 2])
-        self.y = tf.placeholder(tf.float32, [None, 32, 32])
         Wconv1 = tf.get_variable("Wconv1", shape=[5, 5, 2, 256])
         bconv1 = tf.get_variable("bconv1", shape=[256])
         W1 = tf.get_variable("W1", shape=[32, 32, 1, 49])
@@ -37,6 +36,43 @@ class CNN_deblender(object):
         y_out = tf.matmul(W1, h1_flat)
         y_out = tf.transpose(y_out)
         self.y_out = tf.reshape(y_out, [-1, 32, 32]) + b1
+
+    def basic_unit(input_layer, i):
+        with tf.variable_scope("basic_unit" + str(i)):
+            part1 = tf.layers.conv2d(input_layer, 32, [3, 3],
+                                     padding='VALID',
+                                     activation=tf.nn.relu)
+            part2 = tf.layers.batch_norm(part1,
+                                         activation=tf.nn.relu)
+            part3 = tf.layers.conv2d(part2, 32, [3, 3],
+                                     padding='VALID',
+                                     activation=tf.nn.relu)
+            return part3
+
+    def multi_layer_model(self, num_layers):
+        """A 3 layer CNN
+        Conv 3*3*2 s1, 32/ReLU
+        [Conv 3*3 s1, 32/Relu] * 3
+        deconv
+          """
+        layer_in = tf.layers.conv2d(self.X, 32, [3, 3, 2],
+                                    padding='VALID',
+                                    activation=tf.nn.relu,
+                                    scope="conv_0")
+        for i in range(num_layers):
+            layer_in = basic_unit(layer_in, i)
+        top = tf.nn.conv2d_transpose(layer_in, deconv_weights)
+
+
+    def build_net(self):
+        """makes a simple 2 layer CNN
+        layer 1 Conv 5*5*2s2, 256/ReLU
+        layer 2 FC 32*32,1, 49
+         """
+        self.X = tf.placeholder(tf.float32, [None, 32, 32, 2])
+        self.y = tf.placeholder(tf.float32, [None, 32, 32])
+        # Run the preferred CNN model here
+        self.simple_model()
         self.get_mean_loss()
         self.train_step = self.optimizer.minimize(self.mean_loss)
 
@@ -54,7 +90,6 @@ class CNN_deblender(object):
     def run_model(self, X_test, X_train, Y_train,
                   epochs=1, batch_size=64, print_every=100,
                   training=None, plot_losses=False):
-        self.tot_loss = []
         # shuffle indicies
         train_indicies = np.arange(X_train.shape[0])
         np.random.shuffle(train_indicies)
