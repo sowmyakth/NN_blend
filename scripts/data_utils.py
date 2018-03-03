183d31fef313890f6a2249d7036dc6546dfaded1
sowmyakth <sowmya.spacebug@gmail.com>
2018-03-02 18:39:26 -0800

    stamp sizes changed

--- scripts/data_utils.py
+++ scripts/data_utils.py
@@ -18,12 +18,14 @@ def get_stamps(full_image, rows, cols, in_size, out_size):
 
 def load_images(filename, bands):
     """Returns individual postage stamp images of each blend in each band"""
-    in_size, out_size = 240, 32
-    num = 200
+    in_size, out_size = 150, 32
+    num = 2048
     image = np.zeros([num, out_size, out_size, len(bands)])
     for i, band in enumerate(bands):
+        print ("Getting pstamps for band", band)
         full_image = fits.open(filename.replace("band", band))[0].data
-        image.T[i] = get_stamps(full_image, rows=25, cols=8,
+        print (full_image.shape)
+        image.T[i] = get_stamps(full_image, rows=16, cols=128,
                                 in_size=in_size, out_size=out_size)
     return image
 
@@ -52,17 +54,25 @@ def get_train_val_sets(X, Y, subtract_mean, split=0.1):
 
 
 def get_data(subtract_mean=False,
-             normalize_stamps=True):
+             normalize_stamps=True, save_files=False):
     bands = ['i', 'r', 'g']
-    path = os.path.join(os.path.dirname(os.getcwd()), "data")
+    #path = os.path.join(os.path.dirname(os.getcwd()), "data")
+    path = '/global/projecta/projectdirs/lsst/groups/WL/projects/wl-btf/two_gal_blend_data/training_data'
     filename = os.path.join(path, 'gal_pair_band_wldeb.fits')
     X = load_images(filename, bands)
     filename = os.path.join(path, 'central_gal_band_wldeb.fits')
     Y = load_images(filename, ['i'])
     if normalize_stamps:
-        sum_image = X.sum(axis=4).sum(axis=1).sum(axis=1)
+        sum_image = X.sum(axis=3).sum(axis=1).sum(axis=1)
         X = (X.T / sum_image.T).T * 100
         # sum_image = Y.sum(axis=3).sum(axis=1).sum(axis=1)
         Y = (Y.T / sum_image.T).T * 100
     X_train, Y_train, X_val, Y_val = get_train_val_sets(X, Y, subtract_mean)
+    if save_files is True:
+        path = os.path.join(os.path.dirname(os.getcwd()), "data")
+        filename = os.path.join(path, 'training_data')
+        np.savez(filename, X_train=X_train,
+                 Y_train=Y_train, X_val=X_val,
+                 Y_val=Y_val)
+        return
     return X_train, Y_train, X_val, Y_val
